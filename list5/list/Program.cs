403ab9fa96a227df6b4f79cefbff52f04a0b0f1f
list4 <caovantoan95|@gmailo.com>
2016-09-26 08:34:29 +0700

    li1

---
+++ list5/list/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace list
+{
+    class element<T>
+    {
+        private T data
+    
+
+public T Data
+{
+  get { return data; }
+  set { data = value; }
+}}
