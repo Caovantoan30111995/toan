20690e989b539d479263979a421399413b630c0f
list5 <caovantoan95|@gmailo.com>
2016-09-26 09:21:40 +0700

    gfgfgfg

---
+++ list5/list/list1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace list
+{
+    class list1<T>
+    {
+        element<T> Head;
+  
+
+private element<T> Head;
+
+
+
+private element<T> Head1;
+
+internal element<T> Head11
+{
+  get { return Head1; }
+  set { Head1 = value; }
+}
+        public list(){
+        }
+        public void addfirst(element<T> e){
+
+        }
+}
